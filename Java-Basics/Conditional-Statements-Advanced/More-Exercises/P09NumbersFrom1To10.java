259f04714d3d99d53b4f87ce3d62cc05d7ae4328
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-09-19 18:32:28 +0300

    Create P09NumbersFrom1To10.java

---
+++ Java-Basics/Conditional-Statements-Advanced/More-Exercises/P09NumbersFrom1To10.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class NumbersFromOneToTen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
