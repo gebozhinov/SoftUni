e2d6580a105f4b1e8955e0006cd38e325f9c7f44
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-09-20 19:45:38 +0300

    Create P04NumberDividedBy3WithoutReminder.java

---
+++ Java-Basics/While-Loop/More-Exercises/P04NumberDividedBy3WithoutReminder.java
@@ -0,0 +1,8 @@
+public class NumbersDividedByThree {
+    public static void main(String[] args) {
+
+        for (int i = 3; i <= 99; i += 3) {
+            System.out.println(i);
+        }
+    }
+}
