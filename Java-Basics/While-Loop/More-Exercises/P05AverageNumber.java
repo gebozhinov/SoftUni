245a9308d3359ed89a731d2442e8265287bf4d74
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-09-20 19:45:57 +0300

    Create P05AverageNumber.java

---
+++ Java-Basics/While-Loop/More-Exercises/P05AverageNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AverageNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+            sum += number;
+        }
+        double average = sum / n;
+        System.out.printf("%.2f", average);
+    }
+}
