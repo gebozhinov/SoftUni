c7ce12d45ab94d7ba69953be6aba77a914f8ac83
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-09-21 15:01:06 +0300

    Create P10ClockPart2.java

---
+++ Java-Basics/For-Loop/More-Exercises/P10ClockPart2.java
@@ -0,0 +1,12 @@
+public class ClockPartTwo {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 24; i++) {
+            for (int j = 0; j < 60; j++) {
+                for (int k = 0; k < 60; k++) {
+                    System.out.printf("%d : %d : %d%n", i, j, k);
+                }
+            }
+        }
+    }
+}
