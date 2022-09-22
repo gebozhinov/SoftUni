898cab897c852a496a130c9b85e5f801d5dadde7
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-09-22 15:15:18 +0300

    Create P03SquareOfStars.java

---
+++ Java-Basics/Drawing Figures with Loops/P03SquareOfStars.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SquareOfStars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(repeatString("* ", n));
+            System.out.println();
+        }
+
+
+    }
+    static String repeatString(String stringToRepeat, int count) {
+        String text = "";
+        for (int i = 0; i < count; i++) {
+            text += stringToRepeat;
+        }
+        return text;
+    }
+}
