cbb71aade5ef2eb664908a03c7f235d78bd825dc
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-09-23 18:32:06 +0300

    Create P01UniquePINCodes.java

---
+++ Java-Basics/Nested-Loops/More-Exercises/P01UniquePINCodes.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class UniquePinCodes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int n1 = Integer.parseInt(scanner.nextLine());
+        int n2 = Integer.parseInt(scanner.nextLine());
+        int n3 = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= n1; i++) {
+            for (int j = 1; j <= n2; j++) {
+                for (int k = 1; k <= n3; k++) {
+                    if (i % 2 == 0 && k % 2 == 0 && (j == 2 || j == 3 || j == 5 || j == 7)) {
+                        System.out.printf("%d %d %d%n", i, j, k);
+                    }
+                }
+            }
+        }
+    }
+}
