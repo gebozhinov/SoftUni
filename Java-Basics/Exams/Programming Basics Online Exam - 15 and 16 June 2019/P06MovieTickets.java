c1531412a2ebcf6873581388463981fdced934e6
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-10-02 10:33:57 +0300

    Create P06MovieTickets.java

---
+++ Java-Basics/Exams/Programming Basics Online Exam - 15 and 16 June 2019/P06MovieTickets.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class MovieTickets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a1 = Integer.parseInt(scanner.nextLine());
+        int a2 = Integer.parseInt(scanner.nextLine());
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = a1; i <= a2 - 1; i++) {
+            for (int j = 1; j <= n - 1; j++) {
+                for (int k = 1; k <= n / 2 - 1; k++) {
+                    if (i % 2 != 0) {
+                        if ((j + k + i) % 2 != 0) {
+                            System.out.printf("%c-%d%d%d%n", i, j, k, i);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
