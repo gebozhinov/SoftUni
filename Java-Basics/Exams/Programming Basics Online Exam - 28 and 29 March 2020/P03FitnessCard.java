7e4dd5699907768a5dd59a17e5243479f3e76077
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-10-04 15:07:05 +0300

    Create P03FitnessCard.java

---
+++ Java-Basics/Exams/Programming Basics Online Exam - 28 and 29 March 2020/P03FitnessCard.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class FitnessCard {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double budget = Double.parseDouble(scanner.nextLine());
+        String gender = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        String sport = scanner.nextLine();
+
+        double price = 0;
+
+        switch (gender) {
+            case "m":
+                switch (sport) {
+                    case "Gym":
+                        price = 42;
+                        break;
+                    case "Boxing":
+                        price = 41;
+                        break;
+                    case "Yoga":
+                        price = 45;
+                        break;
+                    case "Zumba":
+                        price = 34;
+                        break;
+                    case "Dances":
+                        price = 51;
+                        break;
+                    case "Pilates":
+                        price = 39;
+                        break;
+                }
+                break;
+            case "f":
+                switch (sport) {
+                    case "Gym":
+                        price = 35;
+                        break;
+                    case "Boxing":
+                        price = 37;
+                        break;
+                    case "Yoga":
+                        price = 42;
+                        break;
+                    case "Zumba":
+                        price = 31;
+                        break;
+                    case "Dances":
+                        price = 53;
+                        break;
+                    case "Pilates":
+                        price = 37;
+                        break;
+                }
+                break;
+        }
+        if (age <= 19) {
+            price *= 0.8;
+        }
+        if (budget >= price) {
+            System.out.printf("You purchased a 1 month pass for %s.", sport);
+        } else {
+            System.out.printf("You don't have enough money! You need $%.2f more.", price - budget);
+        }
+    }
+}
