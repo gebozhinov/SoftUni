ee7b2b356f5001ac2641a6b845bf5bad807d55f5
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-10-05 15:04:13 +0300

    Create P08DivisibleBy3.java

---
+++ Java-Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/P08DivisibleBy3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class P08DivisibleBy3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        for (int i = 3; i < 100; i += 3) {
+            System.out.println(i);
+        }
+
+
+    }
+}
