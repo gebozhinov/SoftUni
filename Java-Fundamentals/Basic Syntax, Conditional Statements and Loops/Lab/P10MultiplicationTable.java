ebb820b04d47981d1031512dab141b5a9a27f229
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-10-05 15:04:45 +0300

    Create P10MultiplicationTable.java

---
+++ Java-Fundamentals/Basic Syntax, Conditional Statements and Loops/Lab/P10MultiplicationTable.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class P10MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d X %d = %d%n", n, i, n * i);
+
+        }
+
+
+    }
+}
