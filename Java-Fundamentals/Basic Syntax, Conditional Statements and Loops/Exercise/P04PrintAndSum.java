ca6d746e8b493678cd1ea47a6dda2ac232b4fe6d
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-10-06 21:04:30 +0300

    Create P04PrintAndSum.java

---
+++ Java-Fundamentals/Basic Syntax, Conditional Statements and Loops/Exercise/P04PrintAndSum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class P04PrintAndSum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int startNumber = Integer.parseInt(scanner.nextLine());
+        int endNumber = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        for (int i = startNumber; i <= endNumber; i++) {
+            System.out.print(i + " ");
+            sum += i;
+        }
+        System.out.println();
+        System.out.printf("Sum: %d", sum);
+
+
+    }
+}
