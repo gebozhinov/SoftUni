c8cbc7a198454cb8082d4ca8f4bcb972bfd29341
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-10-07 21:01:25 +0300

    Create P01SortNumbers.java

---
+++ Java-Fundamentals/Basic Syntax, Conditional Statements and Loops/More Exercise/P01SortNumbers.java
@@ -0,0 +1,36 @@
+package basic.syntax.conditional.statements.and.loops.more.exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P01SortNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n1 = Integer.parseInt(scanner.nextLine());
+        int n2 = Integer.parseInt(scanner.nextLine());
+        int n3 = Integer.parseInt(scanner.nextLine());
+
+        int num;
+
+        if (n1 < n2) {
+            num = n1;
+            n1 = n2;
+            n2 = num;
+        }
+        if (n1 < n3) {
+            num = n1;
+            n1 = n3;
+            n3 = num;
+        }
+        if (n2 < n3) {
+            num = n2;
+            n2 = n3;
+            n3 = num;
+        }
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+
+    }
+}
