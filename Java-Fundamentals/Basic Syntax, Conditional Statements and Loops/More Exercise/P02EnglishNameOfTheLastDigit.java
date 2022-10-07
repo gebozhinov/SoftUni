41803d9d4a7dade27447b3fb36af6d0bd07a3ec7
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-10-07 21:01:54 +0300

    Create P02EnglishNameOfTheLastDigit.java

---
+++ Java-Fundamentals/Basic Syntax, Conditional Statements and Loops/More Exercise/P02EnglishNameOfTheLastDigit.java
@@ -0,0 +1,23 @@
+package basic.syntax.conditional.statements.and.loops.more.exercise;
+
+import java.util.Scanner;
+
+public class P02EnglishNameOfTheLastDigit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int number = Integer.parseInt(scanner.nextLine());
+
+        int lastDigit = number % 10;
+        if (lastDigit == 0) {
+            System.out.println("zero");
+        } else {
+            System.out.println(names[lastDigit - 1]);
+        }
+
+    }
+
+    public static final String[] names = {"one", "two", "three", "four", "five", "six", "seven",
+            "eight", "nine"};
+}
