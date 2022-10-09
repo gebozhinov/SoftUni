e3fc5e21ee06c968ff4c43516a83a7d17aa9cff1
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-10-09 13:11:42 +0300

    Create P06TriplesOfLatinLetters.java

---
+++ Java-Fundamentals/Data Types and Variables/Exercise/P06TriplesOfLatinLetters.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TriplesOfLatinLetters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 97; i < 97 + n; i++) {
+            for (int j = 97; j < 97 + n; j++) {
+                for (int k = 97; k < 97 + n; k++) {
+                    System.out.printf("%c%c%c%n", i, j, k);
+
+                }
+            }
+        }
+    }
+}
