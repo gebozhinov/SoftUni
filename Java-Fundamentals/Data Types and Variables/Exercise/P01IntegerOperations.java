1793421efa027c3b23cc75f3d02a456bc6d65c74
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-10-09 13:10:15 +0300

    Create P01IntegerOperations.java

---
+++ Java-Fundamentals/Data Types and Variables/Exercise/P01IntegerOperations.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class IntegerOperations {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int n1 = Integer.parseInt(scanner.nextLine());
+        int n2 = Integer.parseInt(scanner.nextLine());
+        int n3 = Integer.parseInt(scanner.nextLine());
+        int n4 = Integer.parseInt(scanner.nextLine());
+
+        int result = (n1 + n2) / n3 * n4;
+
+        System.out.println(result);
+
+
+    }
+}
