7f62dc9fdbbcde49c0d258f1950fe51aa5b0fedd
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-10-11 18:07:49 +0300

    Create P02PrintNumbersInReverseOrder.java

---
+++ Java-Fundamentals/Arrays/Lab/P02PrintNumbersInReverseOrder.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class P02PrintNumbersInReverseOrder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        int[] numbersArr = new int[n];
+        for (int i = 0; i < n; i++) {
+            int number = Integer.parseInt(scanner.nextLine());
+            numbersArr[i] = number;
+        }
+        for (int i = numbersArr.length - 1; i >= 0; i--) {
+            System.out.print(numbersArr[i] + " ");
+        }
+
+    }
+}
