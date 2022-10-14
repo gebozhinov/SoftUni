07f17f5d7ad655b389793f668081c887bdd11aab
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-10-14 19:06:49 +0300

    Create P02Grades.java

---
+++ Java-Fundamentals/Methods/Lab/P02Grades.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class P02Grades {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        double grade = Double.parseDouble(scanner.nextLine());
+
+        correspondingGradeInWords(grade);
+
+
+    }
+    public static void correspondingGradeInWords(double grade) {
+        if (grade <= 2.99) {
+            System.out.println("Fail");
+        } else if (grade <= 3.49) {
+            System.out.println("Poor");
+        } else if (grade <= 4.49) {
+            System.out.println("Good");
+        } else if (grade <= 5.49) {
+            System.out.println("Very good");
+        } else {
+            System.out.println("Excellent");
+        }
+    }
+}
