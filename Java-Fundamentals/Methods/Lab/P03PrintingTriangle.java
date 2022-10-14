380511c5daa98e19382e1cd6bb31b80987e153bd
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-10-14 19:07:05 +0300

    Create P03PrintingTriangle.java

---
+++ Java-Fundamentals/Methods/Lab/P03PrintingTriangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class P03PrintingTriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int n = Integer.parseInt(scanner.nextLine());
+        printTriangle(n);
+
+
+    }
+    public static void printTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            printLine(i);
+        }
+        for (int i = n - 1; i >= 1 ; i--) {
+            printLine(i);
+        }
+    }
+    public static void printLine(int n) {
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
