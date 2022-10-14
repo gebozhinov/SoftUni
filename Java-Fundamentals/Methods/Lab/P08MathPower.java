27bd75b8ffdb64a1ac31c388a45a8bb66c4eff81
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-10-14 19:08:24 +0300

    Create P08MathPower.java

---
+++ Java-Fundamentals/Methods/Lab/P08MathPower.java
@@ -0,0 +1,24 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class P08MathPower {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double number = Double.parseDouble(scanner.nextLine());
+        double power = Double.parseDouble(scanner.nextLine());
+
+        double result = mathPower(number, power);
+        DecimalFormat df = new DecimalFormat("0.####");
+        System.out.println(df.format(result));
+
+    }
+
+    public static double mathPower(double number, double power) {
+        double result = 1;
+        for (int i = 1; i <= power; i++) {
+            result = result * number;
+        }
+        return result;
+    }
+}
