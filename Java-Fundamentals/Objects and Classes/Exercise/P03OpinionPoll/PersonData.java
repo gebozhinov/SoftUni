3d8df6465061aa1b5d114f302b6178b2ac1229ff
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-10-31 15:56:14 +0200

    Create PersonData.java

---
+++ Java-Fundamentals/Objects and Classes/Exercise/P03OpinionPoll/PersonData.java
@@ -0,0 +1,16 @@
+public class PersonData {
+    private String name;
+    private int age;
+
+
+    public PersonData(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public int getAge() {
+        return this.age;
+    }
+}
