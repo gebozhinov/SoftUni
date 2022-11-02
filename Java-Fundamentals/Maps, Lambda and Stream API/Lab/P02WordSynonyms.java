6fd0de4c69f403d31dea05b3022961d0f4c67156
Georgi Bozhinov <ge.bozhinov@gmail.com>
2022-11-02 19:25:13 +0200

    Create P02WordSynonyms.java

---
+++ Java-Fundamentals/Maps, Lambda and Stream API/Lab/P02WordSynonyms.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class P02WordSynonyms {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        Map<String, List<String>> wordMap = new LinkedHashMap<>();
+        for (int i = 0; i < n; i++) {
+            String word = scanner.nextLine();
+            String synonym = scanner.nextLine();
+            wordMap.putIfAbsent(word, new ArrayList<>());
+            wordMap.get(word).add(synonym);
+
+        }
+        for (Map.Entry<String, List<String>> entry : wordMap.entrySet()) {
+            System.out.printf("%s - %s%n", entry.getKey(), String.join(", ", entry.getValue()));
+        }
+
+    }
+}
