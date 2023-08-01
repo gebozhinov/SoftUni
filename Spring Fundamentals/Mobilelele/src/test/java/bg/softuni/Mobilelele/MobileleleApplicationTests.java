1bed17c0aca0213e32e3bfc7aa1ecbe161185aed
gebozhinov <ge.bozhinov@gmail.com>
2023-08-01 19:25:13 +0300

    starting mobilelele project

---
+++ Spring Fundamentals/Mobilelele/src/test/java/bg/softuni/Mobilelele/MobileleleApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.Mobilelele;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileleleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
