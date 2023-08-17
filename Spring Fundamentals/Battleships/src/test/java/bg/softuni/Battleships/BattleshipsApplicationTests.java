52783195e3c5d750a8b19253bedb3022d8e9fb59
gebozhinov <ge.bozhinov@gmail.com>
2023-08-17 14:50:35 +0300

    Starting Battleships projects - Spring Fundametals exam - 24 Oct 2021

---
+++ Spring Fundamentals/Battleships/src/test/java/bg/softuni/Battleships/BattleshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.Battleships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
