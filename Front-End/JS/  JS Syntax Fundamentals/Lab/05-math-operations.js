dc61f911abaa3056bb13ef6e5cce7d9599bb2933
gebozhinov <ge.bozhinov@gmail.com>
2024-02-20 18:52:58 +0200

    task 05 - math operations

---
+++ Front-End/JS/  JS Syntax Fundamentals/Lab/05-math-operations.js
@@ -0,0 +1,25 @@
+function operation(num1, num2, operator) {
+    switch(operator) {
+        case "+":
+            console.log(num1 + num2);
+            break;
+        case "-":
+            console.log(num1 - num2);
+            break;
+        case "*":
+            console.log(num1 * num2);
+            break;
+        case "/":
+            console.log(num1 / num2);
+            break;
+        case "%":
+            console.log(num1 % num2);
+            break;
+        case "**":
+            console.log(num1 + num2);
+            break;
+    }
+}
+
+operation(5, 6, '+')
+operation(3, 5.5, '*')
