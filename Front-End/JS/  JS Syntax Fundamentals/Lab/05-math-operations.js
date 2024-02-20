4790d6ed8fcf385978a1dffcc8e7947484d55672
gebozhinov <ge.bozhinov@gmail.com>
2024-02-20 18:57:01 +0200

    task 06 - largest number

--- Front-End/JS/  JS Syntax Fundamentals/Lab/05-math-operations.js
+++ Front-End/JS/  JS Syntax Fundamentals/Lab/05-math-operations.js
@@ -16,10 +16,7 @@ function operation(num1, num2, operator) {
             console.log(num1 % num2);
             break;
         case "**":
-            console.log(num1 + num2);
+            console.log(num1 ** num2);
             break;
     }
 }
-
-operation(5, 6, '+')
-operation(3, 5.5, '*')
---
+++ Front-End/JS/  JS Syntax Fundamentals/Lab/06-largest-number.js
@@ -0,0 +1,7 @@
+function printLargestNumber(num1, num2, num3) {
+    let maxNum = Math.max(num1, num2, num3);
+    console.log(`The largest number is ${maxNum}.`);
+}
+
+printLargestNumber(5, -3, 16)
+printLargestNumber(-3, -5, -22.5)
