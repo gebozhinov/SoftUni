24502c7ff0066c983bccea9c9758725676c520af
gebozhinov <ge.bozhinov@gmail.com>
2024-02-20 19:15:06 +0200

    task 07 - theatre promotions

---
+++ Front-End/JS/  JS Syntax Fundamentals/Lab/07-theatre-promotions.js
@@ -0,0 +1,37 @@
+function tickerPrice(typeOfDay, age) {
+    let price;
+    switch (typeOfDay) {
+        case "Weekday":
+            if (age > 18 && age <= 64) {
+                price = 18;
+            } else if (age >= 0 && age <= 122) {
+                price = 12;
+            }
+            break;
+        case "Weekend":
+            if (age > 18 && age <= 64) {
+                price = 20;
+            } else if (age >= 0 && age <= 122) {
+                price = 15;
+            }
+            break;
+        case "Holiday":
+            if (age >= 0 && age <= 18) {
+                price = 5;
+            } else if (age > 18 && age <= 64) {
+                price = 12;
+            } else if (age > 64 && age <= 122) {
+                price = 10;
+            }
+            break;
+    }
+
+    if (typeof price === "number") {
+        console.log(`${price}$`)
+    } else {
+        console.log("Error!")
+    }
+}
+
+tickerPrice('Weekday', 19)
+
