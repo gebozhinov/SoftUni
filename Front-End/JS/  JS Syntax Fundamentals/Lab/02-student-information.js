89ada0ec12d20203c3c6766c2611a47280294c7c
gebozhinov <ge.bozhinov@gmail.com>
2024-02-20 18:38:45 +0200

    task 03 - excellent grade

--- Front-End/JS/  JS Syntax Fundamentals/Lab/02-student-information.js
+++ Front-End/JS/  JS Syntax Fundamentals/Lab/02-student-information.js
@@ -1,8 +1,3 @@
 function studentInfo(name, age, avgGrade) {
     console.log(`Name: ${name}, Age: ${age}, Grade: ${avgGrade.toFixed(2)}`)
 }
-
-
-studentInfo('John', 15, 5.54678)
-studentInfo('Steve', 16, 2.1426)
-studentInfo('Marry', 12, 6.00)
---
+++ Front-End/JS/  JS Syntax Fundamentals/Lab/03-excellent-grade.js
@@ -0,0 +1,10 @@
+function excellent(grade) {
+    if (grade >= 5.50) {
+        console.log("Excellent")
+    } else {
+        console.log("Not excellent")
+    }
+}
+
+excellent(5.50)
+excellent(4.35)
