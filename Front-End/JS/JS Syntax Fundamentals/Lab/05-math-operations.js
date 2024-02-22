39919aa138efe3e35fdde2a75454c8e7edacf297
gebozhinov <ge.bozhinov@gmail.com>
2024-02-22 18:48:55 +0200

    task 01 from exercises - Ages

---
+++ Front-End/JS/JS Syntax Fundamentals/Lab/05-math-operations.js
@@ -0,0 +1,22 @@
+function operation(num1, num2, operator) {
+    switch(operator) {
+        case "+":
+            console.log(num1 + num2);
+            break;
+        case "-":
+            console.log(num1 - num2);
+            break;
+        case "*":
+            console.log(num1 * num2);
+            break;
+        case "/":
+            console.log(num1 / num2);
+            break;
+        case "%":
+            console.log(num1 % num2);
+            break;
+        case "**":
+            console.log(num1 ** num2);
+            break;
+    }
+}
---
+++ Front-End/JS/JS Syntax Fundamentals/Lab/06-largest-number.js
@@ -0,0 +1,4 @@
+function printLargestNumber(num1, num2, num3) {
+    let maxNum = Math.max(num1, num2, num3);
+    console.log(`The largest number is ${maxNum}.`);
+}
