5736767a6ad0ed453815c588285d53cb26e468c6
gebozhinov <ge.bozhinov@gmail.com>
2024-02-25 13:28:28 +0200

    task 05 - censored words

---
+++ Front-End/JS/JS Arrays & Strings /Lab/05-censored-words.js
@@ -0,0 +1,8 @@
+function censored(sentence, word) {
+    let regex = new RegExp((word), 'g');
+    let rep = "*".repeat(word.length);
+
+    console.log(sentence.replace(regex, rep));
+}
+
+censored('Find the hidden word hidden', 'hidden')
