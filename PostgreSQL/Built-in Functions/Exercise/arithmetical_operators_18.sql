84ab4c88d4476973c4b876ebf4aaff52594f1ee0
Georgi Bozhinov <ge.bozhinov@gmail.com>
2024-03-28 20:03:15 +0200

    Create arithmetical_operators_18.sql

---
+++ PostgreSQL/Built-in Functions/Exercise/arithmetical_operators_18.sql
@@ -0,0 +1,7 @@
+CREATE TABLE bookings_calculation
+    AS SELECT
+           booked_for,
+            CAST(booked_for * 50 AS NUMERIC) AS multiplication,
+            CAST(booked_for % 50 AS numeric) AS modulo
+    FROM bookings
+    WHERE apartment_id = 93;
